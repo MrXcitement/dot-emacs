54f80b4df788bc7e7281b36f3a9ebc28fe9a35f1
Mike Barker <mike@thebarkers.com>
2012-10-15 11:21:18 -0600

    Win32: Set the server-auth-dir to the TMP environment variable. I changed the location of the server-auth-dir so that it would not be included in my dropbox folder and be synced across systems.

--- init-server.el
+++ init-server.el
@@ -3,6 +3,14 @@
 ;;                   if it is not allready running.
 ;; Mike Barker <mike@thebarkers.com>
 ;; Copyright (C) 2011 Mike Barker, All rights reserved.
+;; Updates:
+;; 2012.10.15 MRB
+;; * Fixed bug when running under Win32, I was setting the server-socket-dir
+;;   and needed to set the server-auth-dir... It is now set correctly to the
+;;   temp directory defined in the TMP environment variable. I also stopped
+;;   putting it under a folder based on the user id, since I do not know this
+;;   value when running emacsclient and would not be able to tell the client
+;;   where to find the auth file.
 
 ;; Determine the system we are on
 (cond
@@ -17,10 +25,9 @@
 
  ;; Windows customizations
  ((eq system-type 'windows-nt)
-  (setq server-socket-dir (format "%s\\%d" (getenv "TMP")(user-uid)))
-  )
+  (setq server-auth-dir (getenv "TMP"))
  )
-
+)
 ;;;
 ;; Start a server for client processes, do not start if one is allredy running
 (load "server")
