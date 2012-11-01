ed011be731e9f15253d765439fd1108435a3a3f4
Mike Barker <mike@thebarkers.com>
2012-11-01 12:50:08 -0600

    Misc cleanup. wrap require cl in eval-at-compile. suppress byte compile warnings regarding cl. define init--emacs-root with the emacs config dir.

--- init.el
+++ init.el
@@ -1,6 +1,4 @@
-;;;
-;; init.el
-;; My emacs initialization file
+;;; init.el --- My emacs initialization file
 
 ;; This file is NOT part of GNU Emacs
 
@@ -45,29 +43,33 @@
 ;; * moved all settings into this file.
 ;; * using package system with the melpa repository.
 
+;; 2012.11.01 MRB
+;; * wrap require cl in eval-at-compile.
+;;   suppress byte compile warnings regarding cl.
+;;   define init--emacs-root with the emacs config dir.
+
 ;; Save the current time for testing how long the init.el file took to load
 (defvar *init--load-start* (current-time))
 
 ;;;
-;; Load the cl package
-(require 'cl nil t)
+;; Load the cl package and disable byte compile warnings
+(eval-when-compile (require 'cl nil t))
+(setq byte-compile-warnings '(cl-functions))
 
-;;;
-;; Initialize the environment
+;;; Initialize the environment
+(defvar init--emacs-root (expand-file-name "~/.emacs.d"))
 
+;; Add personal directories to the START of load-path
+(add-to-list 'load-path init--emacs-root)
 
-;; If not loading in aquamacs.
 ;; Aquamacs has it's own custom.el and some of the settings in custom.el
 ;; will cause aquamacs to have problems.
 (unless (featurep 'aquamacs)
   ;; Configure and load custom-file used for customize settings.
-  (setq custom-file "~/.emacs.d/custom.el")
+  (setq custom-file (concat init--emacs-root "/custom.el"))
   (load custom-file 'noerror))
 
-;; Add personal directories to the START of load-path
-(add-to-list 'load-path (expand-file-name "~/.emacs.d"))
-
-;; if the user name is not set, set the user full name
+;; if the user name is not set, set it
 (when (eq user-full-name "")
     (setq user-full-name "Mike Barker"))
 
@@ -75,19 +77,19 @@
 (when (eq user-mail-address "")
     (setq user-mail-address "mike@thebarkers.com"))
 
-;; Determine the system we are on
+;; System specific environement settings
 (cond
  ((string-equal "darwin" system-type)
   ;; configure mac os x configuration
   (let ((mypaths '("/Users/mike/bin"
-		  "/Users/Shared/bin"
-		  "/Library/Frameworks/Python.framework/Versions/Current/bin"
-		  "/opt/local/bin"
-		  "/usr/local/git/bin"
-		  "/usr/local/bin"
-		  "/usr/X11/bin"
-		  "/usr/bin"
-		  "/bin")))
+		   "/Users/Shared/bin"
+		   "/Library/Frameworks/Python.framework/Versions/Current/bin"
+		   "/opt/local/bin"
+		   "/usr/local/git/bin"
+		   "/usr/local/bin"
+		   "/usr/X11/bin"
+		   "/usr/bin"
+		   "/bin")))
     (setenv "PATH" (mapconcat 'identity mypaths ":"))
     (setq exec-path mypaths))
   )
@@ -98,16 +100,14 @@
   ;; configure windows environment
   ))
 
-;;;
-;; Initialize the package manager and repositories
+;;; Initialize the package manager and repositories
 (package-initialize)
 (add-to-list 'package-archives 
 	     '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives 
 	     '("melpa" . "http://melpa.milkbox.net/packages/"))
 
-;;;
-;; Initialize default set of external packages
+;; a default set of external packages
 (setq my-packages 
       '(frame-fns frame-cmds zoom-frm
 	redo+
@@ -137,11 +137,7 @@
 ;; mercurial 	                ; Mercurial VC support
 ;; git                          ; GIT mode
 ;; magit
-;; anything                       ; Find anything you want
-;; anything-config
-;; anything-match-plugin
 
-;;;
 ;; Install any of my packages not allready installed
 (defun my-packages-installed-p ()
   (every 'package-installed-p my-packages))
@@ -154,65 +150,57 @@
 	unless (package-installed-p p)
 	do (package-install p)))
 
-;;;
-;; zoom-frm: Only define keys when running as a gui
+;; zoom-frm:
+;; Only define keys when running as a gui
 (when (window-system)
   (global-set-key (kbd "C->") 'zoom-frm-in)
   (global-set-key (kbd "C-<") 'zoom-frm-out)
   (global-set-key (kbd "C-.") 'zoom-frm-unzoom))
 
-;;;
 ;; redo+:
 ;; If you don't want to redo a previous undo, add
 ;; (setq undo-no-redo t)
 (global-set-key (kbd "C-?") 'redo)   ; [Ctrl+Shift+/]
 (global-set-key (kbd "C-x r") 'redo) ; [Ctrl+x r]
 
-;;;
 ;; buffer-move:
 (global-set-key (kbd "C-c <up>")     'buf-move-up)
 (global-set-key (kbd "C-c <down>")   'buf-move-down)
 (global-set-key (kbd "C-c <left>")   'buf-move-left)
 (global-set-key (kbd "C-c <right>")  'buf-move-right)
 
-;;;
 ;; highlight-80+:
 (highlight-80+-mode t)
 
-;;;
 ;; iy-go-to-char:
 (global-set-key (kbd "C-c m") 'iy-go-to-char)
 
-;;;
 ;; csharp-mode:
 (autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
 (setq auto-mode-alist
     (append '(("\\.cs$" . csharp-mode)) auto-mode-alist))
 
-;;;
 ;; ntcmd:
 (autoload 'ntcmd-mode "ntcmd" "Windows batch file mode." t)
 (setq auto-mode-alist
       (append '(("\\.\\(bat\\|cmd\\)$" .
 		 ntcmd-mode)) auto-mode-alist))
 
-;;;
 ;; visual-basic-mode:
-(autoload 'visual-basic-mode "visual-basic-mode" "Visual Basic mode." t)
-(setq auto-mode-alist
-      (append '(("\\.\\(frm\\|bas\\|cls\\|vb\\|vbs\\)$" .
-		 visual-basic-mode)) auto-mode-alist))
+;; (autoload 'visual-basic-mode "visual-basic-mode" "Visual Basic mode." t)
+;; (setq auto-mode-alist
+;;       (append '(("\\.\\(frm\\|bas\\|cls\\|vb\\|vbs\\)$" .
+;; 		 visual-basic-mode)) auto-mode-alist))
 
-;;;
 ;; auto-complete:
 (when (require 'auto-complete-config nil t)
   (ac-config-default))
 
-;;;
 ;; yasnippet:
 (yas-global-mode t)
 
-;;;
+;;; Key mappings
+
 ;; Compilation output, next/previous error. (Alt-Page Up/Alt-Page Down)
 (global-set-key (kbd "<M-prior>") 'previous-error)
 (global-set-key (kbd "<M-next>")  'next-error)
@@ -230,15 +218,14 @@
   ;; Windows key mappings
   ))
 
-;;;
-;; User interface settings 
+;;; User interface settings 
 (setq inhibit-splash-screen t)
 (blink-cursor-mode -1)
 (column-number-mode t)
 (show-paren-mode t)
 (tool-bar-mode -1)
 
-;; Configure window session
+;; Window (gui) ui settings
 (when (window-system)
   (load-theme 'tango t) ; Set the default color theme
   ;; System specific ui settings
@@ -255,12 +242,11 @@
     (set-face-font 'default "Lucida Console 10")
     )))
 
-;; Configure terminal session
+;; Terminal ui settings
 (unless (window-system)
   (menu-bar-mode -1))
 
-;;;
-;; Eshell initialization
+;;; Eshell initialization
 (defmacro with-face (str &rest properties)
   `(propertize ,str 'face (list ,@properties)))
 
@@ -282,23 +268,20 @@
 ;; Needed to tweek for completion to work
 (setq eshell-prompt-regexp "^[^#$\n]*[#$] ")
 
-;;;
-;; Show whitespace in buffer
+;;; Whitespace configuration
 (setq-default show-trailing-whitespace t)
 (setq indicate-empty-lines t)
 ;; Remove trailing whitespace when saving
 ;; (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-;;;
-;; Protect buffers
+;;; Protect buffers
 (save-excursion
   (set-buffer "*scratch*")
   (emacs-lock-mode 'kill)
   (set-buffer "*Messages*")
   (emacs-lock-mode 'kill))
 
-;;;
-;; Autosave and backup support
+;;; Autosave and backup support
 (setq user-temporary-file-directory (expand-file-name "~/tmp/emacs/"))
 (make-directory user-temporary-file-directory t)
 
@@ -314,10 +297,9 @@
 (setq auto-save-file-name-transforms
       `(("\\`/?\\([^/]*/\\)*\\([^/]*\\)\\'" ,user-temporary-file-directory t)))
 
-;;;
-;; Interactively do things
+;;; Interactively do things
 (ido-mode t)
-(setq ido-enable-flex-matching t) ;; enable fuzzy matching
+(setq ido-enable-flex-matching t) ; enable fuzzy matching
 
 ;; Set M-x to use ido mode
 (defun ido-execute-command ()
@@ -329,29 +311,27 @@
       (all-completions "" obarray 'commandp)))))
 (global-set-key (kbd "M-x") 'ido-execute-command)
 
-;;;
-;; Start a server for client processes, do not start if one is allredy running
-;; Determine the system we are on
+;;; Server mode
+
+;; System type server settings
 (cond
  ((eq system-type 'darwin)
-  ;; Darwin (Mac OS X) custimzations
+  ;; Darwin (Mac OS X)
   )
  ((eq system-type 'gnu/linux)
-  ;; Gnu/linux customizations
+  ;; Gnu/linux
   )
  ((eq system-type 'windows-nt)
-  ;; Windows customizations
+  ;; Windows
   (setq server-auth-dir (getenv "TMP"))
   ))
 
-;;;
 ;; Start a server for client processes, do not start if one is allredy running
 (load "server")
 (unless (server-running-p)
   (server-start))
 
-;;;
-;; How long did it take to initialize emacs.
+;;; How long did it take to initialize emacs.
 (message "Emacs initialization (init.el) loaded in %ds"
 	 (destructuring-bind (hi lo ms) (current-time)
 	   (- (+ hi lo) (+ (first *init--load-start*) (second *init--load-start*)))))
