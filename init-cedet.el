fc78e1ef9edf0ee134e956a4686699268c624226
Mike Barker <mike@thebarkers.com>
2012-11-19 17:27:05 -0700

    Initialize the cedet major mode. Currently not used.

---
+++ init-cedet.el
@@ -0,0 +1,20 @@
+;; init-cedet.el -- Initialize the cedet mode 
+
+;; Mike Barker <mike@thebarkers.com>
+;; November 18, 2012
+
+;; Copyright (c) 2012 Mike Barker 
+
+;; Change log:
+;; 2012.11.18
+;; * First release.
+
+;;; Cedet mode
+
+;; Turn on EDE (Project handling mode)
+(global-ede-mode t)
+(semantic-mode t)
+
+(provide 'init-cedet)
+
+
