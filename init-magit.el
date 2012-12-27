01b22b5b42c8541db94dde2af1d4ba32f6f364b9
Mike Barker <mike@thebarkers.com>
2012-12-27 15:25:10 -0700

    Added whitespace toggle and eol changed from DOS to Unix.

--- init-magit.el
+++ init-magit.el
@@ -28,25 +28,46 @@
 ;;; Change log:
 ;; 2012.12.23
 ;; * First release.
+;; * Full screen magit-status by Magnar Sveen
+;;   http://whattheemacsd.com//setup-magit.el-01.html
+
+;; 2012.12.27 MRB
+;; * Whitespace in diffs by Magnar Sveen
+;;   http://whattheemacsd.com/setup-magit.el-02.html
 
 (when (require 'magit nil t)
 
-  ;;; full screen magit-status
-  ;; by: Magnar Sveen
-  ;; at: http://whattheemacsd.com//setup-magit.el-01.html
-
+  ;;; show full screen magit-status
   (defadvice magit-status (around magit-fullscreen activate)
     (window-configuration-to-register :magit-fullscreen)
     ad-do-it
     (delete-other-windows))
-
+  ;;; restore windows when quit magit-status
   (defun magit-quit-session ()
     "Restore the previous window configuration and kills the magit buffer"
     (interactive)
     (kill-buffer)
     (jump-to-register :magit-fullscreen))
 
-  (define-key magit-status-mode-map (kbd "q") 'magit-quit-session))
+  (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+
+  (defun magit-toggle-whitespace ()
+    (interactive)
+    (if (member "-w" magit-diff-options)
+	(magit-dont-ignore-whitespace)
+      (magit-ignore-whitespace)))
+
+  (defun magit-ignore-whitespace ()
+    (interactive)
+    (add-to-list 'magit-diff-options "-w")
+    (magit-refresh))
+
+  (defun magit-dont-ignore-whitespace ()
+    (interactive)
+    (setq magit-diff-options (remove "-w" magit-diff-options))
+    (magit-refresh))
+
+  (define-key magit-status-mode-map (kbd "W") 'magit-toggle-whitespace))
 
 (provide 'init-magit)
 
