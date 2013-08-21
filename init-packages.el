417b53e6beae20cc8520ee7e8f3a5232c20dad7a
MCUser Barker <mcuser@thebarkers.com>
2013-08-21 09:47:20 -0600

    Package initialization cleanup and remove helm package.

--- init-packages.el
+++ init-packages.el
@@ -33,6 +33,12 @@
 ;; 2013-05-28 MRB
 ;; * Edited comments
 
+;; 2013-08-20 MRB
+;; * removed extra packages.
+;; * remove helm, since it does not support emacs 23 and replace with
+;;   smex an ido interface to M-x.
+
+
 ;;; Helper functions: my-package(s)-*
 
 ;; Refresh the package database, but only if the package name is not found.
@@ -42,11 +48,12 @@
       (message "Refreshing the package database")
       (package-refresh-contents))))
 
-;; Install a single package.
-;; Only install a package that is not allready installed
-;; 2013-05-02 MRB - Ignore errors when installing packages.
-;; This allows a system that does not have access to the various package archives to
-;; continue to work and not stop the initialization of emacs.
+;; Install a single package. 
+;; Only install a package that is not allready installed 
+;; 2013-05-02 MRB - Ignore errors when installing packages. This
+;; allows a system that does not have access to the various package
+;; archives to continue to work and not stop the initialization of
+;; emacs.
 (defun my-package-install (my-package)
   (unless (package-installed-p my-package)
     (message "Installing package: %s" my-package)
@@ -75,26 +82,43 @@
 ;; most of the additional packages that I use in my initialization.
 (my-packages-initialize-emacs)
 
-;;; Install and configure packages/modes
+
+;;; Install and configure packages
+
+;;; auto-complete:
+(my-packages-install '(popup auto-complete))
+(when (package-installed-p 'auto-complete)
+  (when (require 'auto-complete-config nil t)
+    (ac-config-default)))
+
 
 ;;; buffer-move:
-(my-package-install 'buffer-move)
+(my-packages-install '(buffer-move))
 (when (package-installed-p 'buffer-move)
-  (global-set-key (kbd "C-c <up>") 
-		  (lambda () (interactive) 
-		    (call-interactively 'buf-move-up)))
-  (global-set-key (kbd "C-c <down>")
-		  (lambda () (interactive) 
-		    (call-interactively 'buf-move-down)))
-  (global-set-key (kbd "C-c <left>") 
-		  (lambda () (interactive) 
-		    (call-interactively 'buf-move-left)))
-  (global-set-key (kbd "C-c <right>")  
-		  (lambda () 
-		    (interactive) (call-interactively 'buf-move-right))))
+  (global-set-key (kbd "C-c <S-up>")     'buf-move-up)
+  (global-set-key (kbd "C-c <S-down>")   'buf-move-down)
+  (global-set-key (kbd "C-c <S-left>")   'buf-move-left)
+  (global-set-key (kbd "C-c <S-right>")  'buf-move-right))
+
+;;; csharp-mode:
+(my-packages-install '(csharp-mode))
+(when (package-installed-p 'csharp-mode)
+  (setq auto-mode-alist
+	(append '(("\\.cs$" . csharp-mode)) auto-mode-alist)))
+
+;;; git-gutter:
+;; show git status in the gutter of the file
+(my-package-install 'git-gutter)
+(when (package-installed-p 'git-gutter)
+  (global-git-gutter-mode t)
+  (global-set-key (kbd "C-c C-g") 'git-gutter:toggle)
+  (global-set-key (kbd "C-c g =") 'git-gutter:popup-hunk)
+  (global-set-key (kbd "C-c g p") 'git-gutter:previous-hunk)
+  (global-set-key (kbd "C-c g n") 'git-gutter:next-hunk)
+  (global-set-key (kbd "C-c g r") 'git-gutter:revert-hunk))
 
 ;;; highlight-80+:
-(my-package-install 'highlight-80+)
+(my-packages-install '(highlight-80+))
 (when (package-installed-p 'highlight-80+)
   (highlight-80+-mode))
 
@@ -102,23 +126,20 @@
 ;; provide the ability to quicly go/jump to a character.
 (my-package-install 'iy-go-to-char)
 (when (package-installed-p 'iy-go-to-char)
-  (global-set-key (kbd "C-c m") 
-		  (lambda () 
-		    (interactive) (call-interactively 'iy-go-to-char)))
-  (global-set-key (kbd "C-c M")
-		  (lambda () (interactive) 
-		    (call-interactively 'iy-go-to-char-backward))))
+  (global-set-key (kbd "C-c m") 'iy-go-to-char)
+  (global-set-key (kbd "C-c M") 'iy-go-to-char-backward))
 
-;;; undo-tree:
-(my-package-install 'undo-tree)
-(when (package-installed-p 'undo-tree)
-  (global-undo-tree-mode))
+;;; magit: 
+;; Git helper mode
+(my-packages-install '(magit))
+(eval-after-load 'magit '(require 'init-magit))
 
-;;; csharp-mode:
-(my-package-install 'csharp-mode)
-(when (package-installed-p 'csharp-mode)
+;;; markdown:
+(my-packages-install '(markdown-mode))
+(when (package-installed-p 'markdown-mode)
   (setq auto-mode-alist
-	(append '(("\\.cs$" . csharp-mode)) auto-mode-alist)))
+	(append '(("\\.\\(text\\|markdown\\|md\\|mdw\\|mdt\\)$" .
+		   markdown-mode)) auto-mode-alist)))
 
 ;;; ntcmd:
 (my-package-install 'ntcmd)
@@ -126,11 +147,11 @@
   (setq auto-mode-alist
 	(append '(("\\.\\(bat\\|cmd\\)$" . ntcmd-mode)) auto-mode-alist)))
 
-;;; markdown:
-(my-package-install 'markdown-mode)
-(when (package-installed-p 'markdown-mode)
-  (setq auto-mode-alist
-	(append '(("\\.\\(markdown\\|md\\|mdw\\|mdt\\)$" . markdown-mode)) auto-mode-alist)))
+;;; php+-mode:
+(my-packages-install '(php+-mode))
+(when (package-installed-p 'php+-mode)
+  (require 'php+-mode)
+  (php+-mode-setup))
 
 ;;; powershell-mode:
 ;; allow you to edit powershell files.
@@ -141,25 +162,27 @@
 	(append '(("\\.ps1$" . powershell-mode)) auto-mode-alist)))
 
 ;;; powershell: 
-;; allow a inferior powershell shell, but only if on windows.
-(when (string-equal "windows-nt" system-type)
-  (my-package-install 'powershell)
-  (when (package-installed-p 'powershell)
-    (require 'powershell nil t)))
+;; allow an inferior powershell shell
+(my-packages-install '(powershell))
+(when (and (package-installed-p 'powershell)
+	   (string-equal "windows-nt" system-type))
+  (require 'powershell nil t))
 
-;;; magit: 
-;; Git mode
-(my-package-install 'magit)
-(eval-after-load "magit"
-  '(progn
-     (require 'init-magit nil t)))
+;;; semx: 
+;; Smex is a M-x enhancement that uses IDO.
+(my-packages-install '(smex))
+(when (package-installed-p 'smex)
+  (smex-initialize)
+  (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+  (global-set-key (kbd "M-x") 'smex)
+  ;;(global-set-key (kbd "M-x") 'smex-major-mode-commands)
+  )
 
-;;; auto-complete:
-;; provide auto complete suggestions
-(my-package-install 'auto-complete)
-(when (package-installed-p 'auto-complete)
-  (require 'auto-complete-config nil t)
-  (ac-config-default))
+;;; undo-tree:
+(my-packages-install '(undo-tree))
+(when (package-installed-p 'undo-tree)
+  ;;(require 'undo-tree nil t)
+  (global-undo-tree-mode))
 
 ;;; yasnippet:
 ;; snippet template engine
@@ -173,181 +196,4 @@
 	      (setq yas-extra-modes 'text-mode)))
   (yas-global-mode t))
 
-;;; helm:
-;; quicksilver/spotlight for emacs, only works in emacs >= ver 24
-(when (>= emacs-major-version 24)
-  (my-packages-install '(helm helm-themes))
-  (when (package-installed-p 'helm)
-    (global-set-key (kbd "C-c h")   'helm-mini)
-    (helm-mode 1)))
-
-;;; php+-mode:
-(my-package-install 'php+-mode)
-(when (package-installed-p 'php+-mode)
-  (eval-after-load "php+-mode"
-    '(progn
-       (require 'php+-mode)
-       (php+-mode-setup))))
-
-;;; git-gutter:
-;; show git status in the gutter of the file
-(my-package-install 'git-gutter)
-(when (package-installed-p 'git-gutter)
-  (global-git-gutter-mode t)
-  (global-set-key (kbd "C-c C-g") 'git-gutter:toggle)
-  (global-set-key (kbd "C-c g =") 'git-gutter:popup-hunk)
-  (global-set-key (kbd "C-c g p") 'git-gutter:previous-hunk)
-  (global-set-key (kbd "C-c g n") 'git-gutter:next-hunk)
-  (global-set-key (kbd "C-c g r") 'git-gutter:revert-hunk))
-
 (provide 'init-packages)
-
-;;; Modes that have been used in the past, but are not loaded now
-;; evernote-mode                ; ** BROKEN ** evernote client
-;; w3m				; ** used by evernote mode ** w3m browser
-;; org-mode			; org-mode customization
-;; haskell			; Haskel mode
-;; lolcode                      ; LOLCode, program like a cat
-;; python-mode			; Python mode
-;; python                       ; Python tools
-;; pymacs                       ; Python emacs bridge
-;; ipython                      ; Interactive python mode
-;; ruby				; Edit ruby files
-;; jscomint                     ; Run javascript files
-;; mode-compile			; Smart compile configuration
-;; ecb				; Emacs Code Browser setup
-;; dvc                          ; Distributed version control
-;; mercurial			; Mercurial VC support
-;; git                          ; GIT mode
-
-;;; Packages that are not loaded
-;; ecb:
-;; (add-to-list 'load-path "~/.emacs.d/site-lisp/ecb-snap/")
-;; (setq stack-trace-on-error t)
-;; (require 'ecb)
-
-;; (set-face-foreground (quote ecb-default-highlight-face) "DimGray")
-;; (set-face-background (quote ecb-default-highlight-face) "grey60")
-
-;; evernote-mode:
-;; (when (require 'evernote-mode)
-;;   (setq evernote-username "mrbarker")
-;;   (setq evernote-enml-formatter-command '("w3m" "-dump" "-I" "UTF8" "-O" "UTF8"))
-;;   (global-set-key "\C-ceb" 'evernote-browser)
-;;   (global-set-key "\C-cec" 'evernote-create-note)
-;;   (global-set-key "\C-ceo" 'evernote-open-note)
-;;   (global-set-key "\C-cep" 'evernote-post-region)
-;;   (global-set-key "\C-ces" 'evernote-search-notes)
-;;   (global-set-key "\C-ceS" 'evernote-do-saved-search)
-;;   (global-set-key "\C-cew" 'evernote-write-note))
-
-;; js-comint:
-;; (when (require 'js-comint)
-;;   ;; Load hs-minor-mode to hide/show blocks
-;;   (add-hook 'js-mode-hook
-;;	    (lambda ()
-;;	      ;; Scan the file for nested code blocks
-;;	      (imenu-add-menubar-index)
-;;	      ;; Activate the folding mode
-;;	      (hs-minor-mode t)))
-;;   ;; Use node as our repl
-;;   (setq inferior-js-program-command "node")
-;;   (setq inferior-js-mode-hook
-;;	(lambda ()
-;;	  ;; We like nice colors
-;;	  (ansi-color-for-comint-mode-on)
-;;	  Deal with some prompt nonsense
-;;	  (add-to-list 'comint-preoutput-filter-functions
-;;		       (lambda (output)
-;;			 (replace-regexp-in-string ".*1G\.\.\..*5G" "..."
-;;						   (replace-regexp-in-string ".*1G.*3G" ">" output)))))))
-
-;; org-mode:
-;; (add-to-list 'load-path (expand-file-name "~/.emacs.d/site-lisp/org-6.34c/lisp") t)
-;; (require 'org-install)
-
-;; (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-;; (define-key global-map "\C-cl" 'org-store-link)
-;; (define-key global-map "\C-ca" 'org-agenda)
-;; (setq org-log-done t)
-
-;; ;; Set to the location of your Org files on your local system
-;; (setq org-directory "~/Dropbox/Personal/Org")
-;; (setq org-default-notes-file (concat org-directory "/notes.org"))
-
-;; ;; Set to the name of the file where new notes will be stored
-;; (setq org-mobile-inbox-for-pull (concat org-directory "/flagged.org"))
-
-;; ;; Set to <your Dropbox root directory>/MobileOrg.
-;; (setq org-mobile-directory "~/Dropbox/MobileOrg")
-
-;; ;; We need to update the checksum file to only transfer updated files.
-;; (add-hook 'org-mobile-post-push-hook
-;;        (lambda () (shell-command "find ~/Dropbox/MobileOrg -name '*.org' -type f -print | sed 's/^\\.\\///' | xargs md5 > ~/Dropbox/MobileOrg/checksums.dat")))
-
-;; ;; Remember mode integration
-;; (org-remember-insinuate)
-;; (define-key global-map "\C-cr" 'org-remember)
-;; (setq org-remember-templates
-;;      '(
-;;       ("Todo" ?t "* TODO %^{Brief Description} %^g\n%?\nAdded: %U" (concat org-directory "/tasks.org") "Tasks")
-;;       ("Note" ?n "\n* %^{topic} %T \n%i%?\n" (concat org-directory "/notes.org"))
-;;       ("Journal" ?j "\n* %U \n%i%?\n" (concat org-directory "/journal.org"))
-;;       ))
-
-;; (setq org-refile-targets (quote (("notes.org" :level . 1)
-;;				 ("someday.org" :level . 2)
-;;				 ("tasks.org" :maxlevel . 1)
-;;				 )))
-
-;; ;; Personal agenda files
-;; ;; ===================================================================
-;; ;; calendar.org       Birthdays, Aniverseries, etc  <-> Google Calendar
-;; ;; journal.org        Daily information             <-> Evernote
-;; ;; notes.org          Random notes                  <-> Evernote
-;; ;; someday.org        Tasks to be done someday      <-> RTM
-;; ;; tasks.org          Projects and tasks            <-> RTM
-;; (setq org-agenda-files (list (concat org-directory "/calendar.org")
-;;			     (concat org-directory "/journal.org")
-;;			     (concat org-directory "/notes.org")
-;;			     (concat org-directory "/someday.org")
-;;			     (concat org-directory "/tasks.org")
-;;			     ))
-
-;; org-feed
-;; (setq org-feed-alist
-;;       '(("Remember The Milk"
-;;          "https://www.rememberthemilk.com/atom/mrbarker/"
-;;          "~/Dropbox/Personal/Org/rtm.org" "Remember The Milk Entries"
-;;	 :parse-feed org-feed-parse-atom-feed
-;;	 :parse-entry org-feed-parse-rtm-entry
-;;	 )))
-
-;; (defun org-feed-parse-rtm-entry (entry)
-;;   "Parse the `:item-full-text' as a sexp and create new properties."
-;;   (let ((xml (car (read-from-string (plist-get entry :item-full-text)))))
-;;     ;; Get first <link href='foo'/>.
-;;     (setq entry (plist-put entry :link
-;;                            (xml-get-attribute
-;;                             (car (xml-get-children xml 'link))
-;;                             'href)))
-;;     ;; Add <title/> as :title.
-;;     (setq entry (plist-put entry :title
-;;                            (car (xml-node-children
-;;                                  (car (xml-get-children xml 'title))))))
-;;     (let* ((content (car (xml-get-children xml 'content)))
-;;            (type (xml-get-attribute-or-nil content 'type)))
-;;       (when content
-;;         (cond
-;;          ((string= type "text")
-;;           ;; We like plain text.
-;;           (setq entry (plist-put entry :description (car (xml-node-children content)))))
-;;          ((string= type "html")
-;;           ;; TODO: convert HTML to Org markup.
-;;           (setq entry (plist-put entry :description (car (xml-node-children content)))))
-;;          ((string= type "xhtml")
-;;           ;; TODO: convert XHTML to Org markup.
-;;           (setq entry (plist-put entry :description (prin1-to-string (xml-node-children content)))))
-;;          (t
-;;           (setq entry (plist-put entry :description (format "Unknown '%s' content." type)))))))
-;;     entry))
