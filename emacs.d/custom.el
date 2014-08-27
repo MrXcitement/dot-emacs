ab0be786bd72f7a38806894cc17115ac46c1fcef
Mike Barker <mike@thebarkers.com>
2014-08-27 14:49:59 -0600

    changed default browser from macosx-browser to default

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -5,11 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(ac-sources (quote (ac-source-imenu ac-source-abbrev ac-source-words-in-buffer ac-source-files-in-current-dir ac-source-filename)) t)
  '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector (vector "#eaeaea" "#d54e53" "#b9ca4a" "#e7c547" "#7aa6da" "#c397d8" "#70c0b1" "#000000"))
  '(anything-command-map-prefix-key "C-c a")
  '(auto-mode-case-fold t)
- '(browse-url-browser-function (quote browse-url-default-macosx-browser))
- '(custom-safe-themes (quote ("4598cadf7d6b86689307dc9e8fa6ab141bccd950" "967c58175840fcea30b56f2a5a326b232d4939393bed59339d21e46cf4798ecf" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "117284df029007a8012cae1f01c3156d54a0de4b9f2f381feab47809b8a1caef" "e9cd13d22eab8a441c0c7d319dc044881723395b4aa22f283d581d921cc94a87" "0174d99a8f1fdc506fa54403317072982656f127" "5600dc0bb4a2b72a613175da54edb4ad770105aa" "9cdf9fb94f560902b567b73f65c2ed4e5cfbaafe" default)))
+ '(browse-url-browser-function (quote browse-url-default-browser))
+ '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "2b5aa66b7d5be41b18cc67f3286ae664134b95ccc4a86c9339c886dfd736132d" "146d24de1bb61ddfa64062c29b5ff57065552a7c4019bee5d869e938782dfc2a" "70cf411fbf9512a4da81aa1e87b064d3a3f0a47b19d7a4850578c8d64cac2353" "4598cadf7d6b86689307dc9e8fa6ab141bccd950" "967c58175840fcea30b56f2a5a326b232d4939393bed59339d21e46cf4798ecf" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "117284df029007a8012cae1f01c3156d54a0de4b9f2f381feab47809b8a1caef" "e9cd13d22eab8a441c0c7d319dc044881723395b4aa22f283d581d921cc94a87" "0174d99a8f1fdc506fa54403317072982656f127" "5600dc0bb4a2b72a613175da54edb4ad770105aa" "9cdf9fb94f560902b567b73f65c2ed4e5cfbaafe" default)))
  '(custom-theme-directory "~/.emacs.d/themes")
  '(ecb-layout-window-sizes nil)
  '(ecb-options-version "2.40")
