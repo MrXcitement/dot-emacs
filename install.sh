6c0ffbc38c75e0da9bffc03fface2b3e48b6621c
Mike Barker <mike@thebarkers.com>
2014-11-12 11:37:50 -0700

    Fixup install script to install osx stub files.

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@
 # Mike Barker <mike@thebarkers.com>
 # May 14th, 2013
 
+# History:
+# 2014.11.12 MRB
+# * Added code to install scripts based on system type.
+
 echo "$(basename $0)"
 echo "Make sympolic links in the users home directory to the bash config files and directories"
 
@@ -20,3 +24,9 @@ for FILE in ${FSPEC}; do
 	ln -sv ${DIR}/${FILE} ~/.${FILE}
     fi
 done
+
+# handle system specific install
+unamestr=`uname`
+if [[ "$unamestr" == 'Darwin' ]]; then
+    install -v -m770 osx/* ~/bin
+fi
