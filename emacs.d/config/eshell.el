2038610c8b2674b2675218d3c86bdcfb221f1542
Mike Barker <mike@thebarkers.com>
2014-11-13 07:49:00 -0700

    Change how git info is displayed. Only add git info to the prompt on non windows systems. Changed the function names to be more descriptive. Added a newline before the git info section. Clean up the history configuration section.

--- emacs.d/config/eshell.el
+++ emacs.d/config/eshell.el
@@ -18,14 +18,15 @@
 (setq eshell-prompt-regexp "^[^#$\n]*[#$] ")
 
 ;;; History settings
+;; make sure the history vars are defined
+(load "em-hist")
 (setq eshell-history-size 1024)
-(load "em-hist") ;; So the history vars are defined
 (if (boundp 'eshell-save-history-on-exit)
     (setq eshell-save-history-on-exit t))
 (if (boundp 'ehsell-ask-to-save-history)
     (setq eshell-ask-to-save-history 'always))
 
-(defun mrb:pwd-repl-home (pwd)
+(defun mrb:pwd-tilde-for-home (pwd)
   "Returns a path with the home directory replaced with a tilde"
   (interactive)
   (let* ((home (expand-file-name (getenv "HOME")))
@@ -35,21 +36,24 @@
 	(concat "~" (substring pwd home-len)) pwd)))
 
 
-(defun mrb:curr-dir-git-branch-string (pwd)
-  "Returns current git branch as a string, or the empty string if
-PWD is not in a git repo (or the git command is not found)."
+(defun mrb:pwd-current-git-branch (pwd)
+  "When not running on windows.
+Return the current git branch as a string,
+or the empty string if pwd is not in a git repo,
+or the git command is not found."
   (interactive)
-  (if (and (eshell-search-path "git")
+  (when (not (eq system-type 'windows-nt))
+    (if (and (eshell-search-path "git")
 	     (locate-dominating-file pwd ".git"))
-      (progn
-	(let ((git-output (shell-command-to-string
-			   (concat "cd " pwd " && git branch | grep '\\*' | sed -e 's/^\\* //'"))))
-	  (concat "("
-		  (if (> (length git-output) 0)
-		      (substring git-output 0 -1)
-		    "(no branch)")
-		  ")")))
-    (concat "")))
+	(progn
+	  (let ((git-output (shell-command-to-string
+			     (concat "cd " pwd " && git branch | grep '\\*' | sed -e 's/^\\* //'"))))
+	    (concat "("
+		    (if (> (length git-output) 0)
+			(substring git-output 0 -1)
+		      "(no branch)")
+		    ")")))
+      (concat ""))))
 
 ;;; Configure the prompt
 ;; Put a new line after the path and before the prompt
@@ -60,10 +64,10 @@ PWD is not in a git repo (or the git command is not found)."
 	 "@"
 	 (system-name)
 	 ": "
-	 (mrb:pwd-repl-home(eshell/pwd))
-	 " "
-	 (mrb:curr-dir-git-branch-string(eshell/pwd))
+	 (mrb:pwd-tilde-for-home(eshell/pwd))
 	 "\n"
-	 (if (= (user-uid) 0) "# " "$ "))))
+	 (mrb:pwd-current-git-branch(eshell/pwd))
+	 (if (= (user-uid) 0) "#" "$")
+	 " ")))
 
 ;;; eshell.el ends here.
