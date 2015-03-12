ceceb11e0105a60a4b152e9176e0257b480ca5a8
Mike Barker <mike@thebarkers.com>
2015-03-12 14:54:25 -0600

    Added cider mode if lien command exists. For those machines that have lieningen installed, install and configure cider.

---
+++ emacs.d/init.d/packages.d/cider.el
@@ -0,0 +1,10 @@
+;;; cider:
+
+;; Clojure Interactive Development Environment that Rocks for Emacs,
+;; built on top of nREPL, the Clojure networked REPL server.
+;; https://github.com/clojure-emacs/cider
+
+;; When the leim command is available, install/config cider
+(when (mrb:eshell-command-exist-p "lein")
+  (use-package cider
+    :ensure t))
