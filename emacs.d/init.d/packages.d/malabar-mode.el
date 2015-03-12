2a43e8dff34666e82b6788d932d12846e7fe9da1
Mike Barker <mike@thebarkers.com>
2015-03-12 15:32:15 -0600

    use malabar mode, only if mvn command exists. Malabar mode is a Java mode built around the Maven project system. This mode will only be loaded if the mvn command is installed and available.

--- emacs.d/init.d/packages.d/malabar-mode.el
+++ emacs.d/init.d/packages.d/malabar-mode.el
@@ -3,16 +3,16 @@
 ;; Provide support for editing java source and integration with Maven
 ;; for handling projects.
 
-(use-package malabar-mode
-  :if (mrb:eshell-command-exist-p "mvn")
-  :ensure t
-  :mode "\\.java\\'"
-  :config
-  (progn
-    (setq semantic-default-submodes
-	  '(global-semantic-idle-scheduler-mode
-	    global-semanticdb-minor-mode
-	    global-semantic-idle-summary-mode
-	    global-semantic-mru-bookmark-mode))
-    (semantic-mode 1)
-    (setq malabar-groovy-lib-dir "~/lib/malabar/lib")))
+(when (mrb:eshell-command-exist-p "mvn")
+  (use-package malabar-mode
+    :ensure t
+    :mode "\\.java\\'"
+    :config
+    (progn
+      (setq semantic-default-submodes
+	    '(global-semantic-idle-scheduler-mode
+	      global-semanticdb-minor-mode
+	      global-semantic-idle-summary-mode
+	      global-semantic-mru-bookmark-mode))
+      (semantic-mode 1)
+      (setq malabar-groovy-lib-dir "~/lib/malabar/lib"))))
