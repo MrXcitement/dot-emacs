7ce848c38529f77185e49c4470f780d3fa3fbca2
Mike Barker <mike@gopher.thebarkers.lan>
2015-06-27 11:27:49 -0600

    Added evil-mode, emacs is now vim

---
+++ emacs.d/init.d/packages.d/evil-mode.el
@@ -0,0 +1,10 @@
+;;; evil-mode --- install and configure the evil-mode plugin
+
+;; Mike Barker <mike@thebarkers.com>
+;; June 27th, 2015
+
+(use-package evil
+  :ensure
+  :config
+  (progn
+    (evil-mode 1)))
