db4c63a9edeefcfa58253ce31017b49fd96c6b68
Mike Barker <mike@thebarkers.com>
2015-11-06 13:59:58 -0700

    Minor changes
    I removed the eshell functions, since emacs provides executable-find
    that can be used instead.
    I removed the TODO comments, since byte compiling should not occur
    within the load directory function.

--- emacs.d/init.d/init-defuns.el
+++ emacs.d/init.d/init-defuns.el
@@ -8,26 +8,11 @@
 ;; This file is not part of GNU Emacs.
 
 
-;;; Eshell functions
-;; I use the internal eshell to allow cross platform shell script
-(defun mrb:eshell-command-to-string (command)
-  "Run an eshell COMMAND and return the results as a string."
-  (with-temp-buffer
-    (eshell-command command t)
-    (buffer-string)))
-
-(defun mrb:eshell-command-exist-p (command)
-  "Does a specified COMMAND exist on this machine."
-  (not (string-equal (mrb:eshell-command-to-string (concat "which " command)) "")))
-
 ;; In an effort to make my emacs configuration more modular I am using
 ;; the information found at the Emacs Wiki's Dot Emacs Modular article,
 ;; http://www.emacswiki.org/emacs-en/DotEmacsModular
 ;; I can now add initialization files configuration directory and they
 ;; will be loaded at startup.
-
-;; TODO: Add the capability to byte compile updated files and load the
-;; byte-compiled file
 
 ;;; Load all .el files in a directory.
 (defun mrb:load-directory (directory)
