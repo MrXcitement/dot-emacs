162e57ed0a78dcef2ae16fd7d91c59fa0cd320b2
Mike Barker <mike@thebarkers.com>
2016-01-28 11:18:07 -0700

    Replace ido/smex with ivy/swiper mode

--- emacs.d/init.d/packages.d/smex.el
+++ emacs.d/init.d/packages.d/smex.el
@@ -2,11 +2,11 @@
 
 ;; Provide a smart M-x enhancement for Emacs using Ido.
 
-(use-package smex
-  :ensure t
-  :bind
-  (("M-x" . smex)
-   ("M-X" . smex-major-mode-commands)
-   ("C-c M-x" . execute-extended-command))
-  :config
-  (smex-initialize))
+;; (use-package smex
+;;   :ensure t
+;;   :bind
+;;   (("M-x" . smex)
+;;    ("M-X" . smex-major-mode-commands)
+;;    ("C-c M-x" . execute-extended-command))
+;;   :config
+;;   (smex-initialize))
---
+++ emacs.d/init.d/packages.d/swiper.el
@@ -0,0 +1,35 @@
+;;; swiper.el
+
+;; This file loads swiper and related packages.
+;; The package swiper includes ivy-mode. 'Ivy is a generic completion
+;; method for Emacs, similar to icomplete-mode. It aims to be more
+;; efficient, more simple, and more pleasant to use than the
+;; alternatives. It's also highly customizable and very small.'
+
+(unless (executable-find "ag")
+  (message "%s" "executable: ag not found!, counsel-ag will not work."))
+
+(use-package flx
+  :ensure t)
+
+(use-package counsel
+  :ensure t)
+
+(use-package swiper
+  :ensure t
+  :bind
+  (("C-s" . swiper)
+   ("M-x" . counsel-M-x)
+   ("C-x C-f" . counsel-find-file)
+   ("C-c g" . counsel-git)
+   ("C-c j" . counsel-git-grep)
+   ("C-c k" . counsel-ag)
+   ("C-c l" . counsel-locate)
+   ("C-c u" . counsel-unicode-char)
+   ("C-c M-x" . execute-extended-command))
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-initial-inputs-alist nil)
+  (setq ivy-re-builders-alist
+	'((t . ivy--regex-fuzzy))))
